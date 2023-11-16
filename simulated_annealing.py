aac1a8daf1ba3be53d50c8f98f733bd8bac94e7b
Sabyasachi Bhoi <sabyabhoi@gmail.com>
2023-11-16 22:37:40 +0530

    Add simulated annealing

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -5,13 +5,13 @@ import math
 
 class SimulatedAnnealing:
 
-    def __init__(self, start_state: np.ndarray, T, Tmin, k, n, f, constraint,
+    def __init__(self, start_state: np.ndarray, T, Tmin, k, n: int, f, constraint,
                  min_state: np.ndarray):
         self.state = start_state
         self.T = T
         self.Tmin = Tmin
         self.k = k
-        self.n = n
+        self.n = n # no. of iterations
         self.f = f
         self.constraint = constraint
         self.min_state = min_state
@@ -19,19 +19,31 @@ class SimulatedAnnealing:
 
     def neighbour(self, state):
         n = len(state)
-        return (state + np.random.normal(0, 1, n).reshape(n, 1)) / n
+        x, y, z = state
+        x += np.random.randint(-2, 2)
+        x = min(x, 3)
+        x = max(x, 1)
+        y += np.random.randint(-2, 2)
+        y = min(y, 10)
+        y = max(y, 2)
+        z += np.random.randint(-2, 2)
+        z = min(z, 10)
+        z = max(z, 2)
+        return (x, y, z)
 
     def run(self):
         while self.T > self.Tmin:
-            for _ in range(self.n):
+            for i in range(self.n):
+                print(f"[TEMP = {self.T}] {i + 1}: ")
                 if self.f(self.state) > self.f(self.min_state):
                     self.min_state = self.state
-
+                
                 new_state = self.neighbour(self.state)
+
                 change_in_energy = self.f(new_state) - self.f(self.state)
                 exp_term = math.exp(change_in_energy / self.T)
 
-                if self.constraint(new_state) and (change_in_energy > 0
+                if (change_in_energy > 0
                                                    or exp_term
                                                    >= random.uniform(0, 1)):
                     self.state = new_state
